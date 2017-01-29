1a5a950e02b38882b31138aec09b316237709d4f
Jordan <jordandlee@hotmail.ca>
2017-01-29 03:48:57 -0500

    New branch jordan

--- python_files/board_utils.py
+++ python_files/board_utils.py
@@ -1,23 +1,23 @@
 import sys
-from pprint import pprint 
+from pprint import pprint
 
 def opposite(currColour):
     if currColour == '1':
         return '2'
     return '1'
-	
+
 # this will check if the entire 9x9 board is full.
 emptynum = 0
 
 def board_is_full(board):
-	for sub_row in board:
-		for sub_board in sub_row:
-			for row in sub_board:
-				for char in row:
-					if char == emptynum:
-						return False
+    for sub_row in board:
+        for sub_board in sub_row:
+            for row in sub_board:
+                for char in row:
+                    if char == emptynum:
+                        return False
 
-	return True
+    return True
 
 # this will check if a sub board is playable.
 # returns characters
@@ -26,48 +26,48 @@ def board_is_full(board):
 # if board is full it will return 'F'
 def sub_board_is_full(board, move):
 
-	play_col = move % 3
-	play_row = move / 3
-	sub_board = board[play_row][play_col]
-	# vertical
-	if (sub_board[0][0] == sub_board[0][1] and sub_board[0][1] == sub_board[0][2] and sub_board[0][0] != emptynum):
-		return sub_board[0][0];
-	if (sub_board[1][0] == sub_board[1][1] and sub_board[1][1] == sub_board[1][2] and sub_board[1][0] != emptynum):
-		return sub_board[1][0];
-	if (sub_board[2][0] == sub_board[2][1] and sub_board[2][1] == sub_board[2][2] and sub_board[2][0] != emptynum):
-		return sub_board[2][0];
+    play_col = move % 3
+    play_row = move / 3
+    sub_board = board[play_row][play_col]
+    # vertical
+    if (sub_board[0][0] == sub_board[0][1] and sub_board[0][1] == sub_board[0][2] and sub_board[0][0] != emptynum):
+        return sub_board[0][0];
+    if (sub_board[1][0] == sub_board[1][1] and sub_board[1][1] == sub_board[1][2] and sub_board[1][0] != emptynum):
+        return sub_board[1][0];
+    if (sub_board[2][0] == sub_board[2][1] and sub_board[2][1] == sub_board[2][2] and sub_board[2][0] != emptynum):
+        return sub_board[2][0];
 
-	# horizontal
-	if (sub_board[0][0] == sub_board[1][0] and sub_board[1][0] == sub_board[2][0] and sub_board[0][0] != emptynum):
-		return sub_board[0][0];
-	if (sub_board[0][1] == sub_board[1][1] and sub_board[1][1] == sub_board[2][1] and sub_board[0][1] != emptynum):
-		return sub_board[0][1];
-	if (sub_board[0][2] == sub_board[1][2] and sub_board[1][2] == sub_board[2][2] and sub_board[0][2] != emptynum):
-		return sub_board[0][2];
+    # horizontal
+    if (sub_board[0][0] == sub_board[1][0] and sub_board[1][0] == sub_board[2][0] and sub_board[0][0] != emptynum):
+        return sub_board[0][0];
+    if (sub_board[0][1] == sub_board[1][1] and sub_board[1][1] == sub_board[2][1] and sub_board[0][1] != emptynum):
+        return sub_board[0][1];
+    if (sub_board[0][2] == sub_board[1][2] and sub_board[1][2] == sub_board[2][2] and sub_board[0][2] != emptynum):
+        return sub_board[0][2];
 
-	# diagonals
-	if (sub_board[0][0] == sub_board[1][1] and sub_board[1][1] == sub_board[2][2] and sub_board[1][1] != emptynum):
-		return sub_board[0][0];
-	if (sub_board[2][0] == sub_board[1][1] and sub_board[1][1] == sub_board[0][2] and sub_board[1][1] != emptynum):
-		return sub_board[2][0];
+    # diagonals
+    if (sub_board[0][0] == sub_board[1][1] and sub_board[1][1] == sub_board[2][2] and sub_board[1][1] != emptynum):
+        return sub_board[0][0];
+    if (sub_board[2][0] == sub_board[1][1] and sub_board[1][1] == sub_board[0][2] and sub_board[1][1] != emptynum):
+        return sub_board[2][0];
 
-	for row in sub_board:
-		for char in row:
-			if char == emptynum:
-				return char
+    for row in sub_board:
+        for char in row:
+            if char == emptynum:
+                return char
 
-	return 'F'
+    return 'F'
 
 
 def printboard(board):
-	for i in range(3):
-		for k in range(3):
-			for j in range(3):
-				for l in range(3):
-					print str(board[i][j][k][l]),
-				print " ",
-			print " "
-		print " "
+    for i in range(3):
+        for k in range(3):
+            for j in range(3):
+                for l in range(3):
+                    print str(board[i][j][k][l]),
+                print " ",
+            print " "
+        print " "
 
 # returns a 2-D array of valid moves
 # the array will be 3 by 3 size type int
@@ -81,59 +81,59 @@ def printboard(board):
 # 1 is available but undesireable,
 # 0 is impossible
 def get_valid_moves(board, move):
-	if move == 9:
-		valid_moves = [[[[2 for x in range(3)]for y in range(3)] for j in range(3)] for k in range(3)]
-		for board_row_num in range(3):
-			for board_col_num in range(3):
+    if move == 9:
+        valid_moves = [[[[2 for x in range(3)]for y in range(3)] for j in range(3)] for k in range(3)]
+        for board_row_num in range(3):
+            for board_col_num in range(3):
 
-				# removes already played positions
-				sub_board = board[board_row_num][board_col_num]
-				for row_num in range(0,3):
-					for col_num in range(0,3):
-						# if the position is already taken
-						if sub_board[row_num][col_num] != emptynum:
-							valid_moves[board_row_num][board_col_num][row_num][col_num] = 0
-						
-						# if the position is playable,
-						if sub_board[row_num][col_num] == emptynum:
-							next_sub_move = row_num + (3 * col_num)
-							# if the leading sub_board is won/full
-							if sub_board_is_full(board, next_sub_move) != emptynum:
-								valid_moves[board_row_num][board_col_num][row_num][col_num] = 1
-				
-				# checks if the board is done playing. i.e. full or won
-				next_move = board_row_num + 3*board_col_num
-				if sub_board_is_full(board, next_move) != emptynum:
-					for row_num in range(3):
-						for col_num in range(3):
-							valid_moves[board_row_num][board_col_num][row_num][col_num] = 0
-		for i in range(3):
-			for j in range(3):
-				valid_moves[i][j] = zip(*valid_moves[i][j])
-		valid_moves = zip(*valid_moves)
-		return True, valid_moves
+                # removes already played positions
+                sub_board = board[board_row_num][board_col_num]
+                for row_num in range(0,3):
+                    for col_num in range(0,3):
+                        # if the position is already taken
+                        if sub_board[row_num][col_num] != emptynum:
+                            valid_moves[board_row_num][board_col_num][row_num][col_num] = 0
+
+                        # if the position is playable,
+                        if sub_board[row_num][col_num] == emptynum:
+                            next_sub_move = row_num + (3 * col_num)
+                            # if the leading sub_board is won/full
+                            if sub_board_is_full(board, next_sub_move) != emptynum:
+                                valid_moves[board_row_num][board_col_num][row_num][col_num] = 1
+
+                # checks if the board is done playing. i.e. full or won
+                next_move = board_row_num + 3*board_col_num
+                if sub_board_is_full(board, next_move) != emptynum:
+                    for row_num in range(3):
+                        for col_num in range(3):
+                            valid_moves[board_row_num][board_col_num][row_num][col_num] = 0
+        for i in range(3):
+            for j in range(3):
+                valid_moves[i][j] = zip(*valid_moves[i][j])
+        valid_moves = zip(*valid_moves)
+        return True, valid_moves
 
 
-	play_row = move / 3
-	play_col = move % 3
+    play_row = move / 3
+    play_col = move % 3
 
-	valid_moves = [[2 for x in range(3)] for y in range(3)]
+    valid_moves = [[2 for x in range(3)] for y in range(3)]
 
-	sub_board = board[play_row][play_col]
+    sub_board = board[play_row][play_col]
 
-	for row_num in range(3):
-		for col_num in range(3):
-			# if empty
-			if sub_board[row_num][col_num] == emptynum:
+    for row_num in range(3):
+        for col_num in range(3):
+            # if empty
+            if sub_board[row_num][col_num] == emptynum:
 
-				if sub_board_is_full(board, move) == emptynum:
-					valid_moves[row_num][col_num] = 2
-				else:
-					valid_moves[row_num][col_num] = 1
-			else:
-				valid_moves[row_num][col_num] = 0
+                if sub_board_is_full(board, move) == emptynum:
+                    valid_moves[row_num][col_num] = 2
+                else:
+                    valid_moves[row_num][col_num] = 1
+            else:
+                valid_moves[row_num][col_num] = 0
 
-	return False, valid_moves
+    return False, valid_moves
 
 
 # returns the character of who won the game
@@ -141,37 +141,37 @@ def get_valid_moves(board, move):
 # returns 'F' if board was full, and no one has won.
 def who_won(board):
 
-	result_board = [[sub_board_is_full(board, x + 3*y) for x in range(0,3)] for y in range(0,3)]
+    result_board = [[sub_board_is_full(board, x + 3*y) for x in range(0,3)] for y in range(0,3)]
 
 
-	if (result_board[0][0] == result_board[0][1] and result_board[0][1] == result_board[0][2] and sub_board[0][0] != emptynum):
-		return result_board[0][0];
-	if (result_board[1][0] == result_board[1][1] and result_board[1][1] == result_board[1][2] and sub_board[1][0] != emptynum):
-		return result_board[1][0];
-	if (result_board[2][0] == result_board[2][1] and result_board[2][1] == result_board[2][2] and sub_board[2][0] != emptynum):
-		return result_board[2][0];
+    if (result_board[0][0] == result_board[0][1] and result_board[0][1] == result_board[0][2] and sub_board[0][0] != emptynum):
+        return result_board[0][0];
+    if (result_board[1][0] == result_board[1][1] and result_board[1][1] == result_board[1][2] and sub_board[1][0] != emptynum):
+        return result_board[1][0];
+    if (result_board[2][0] == result_board[2][1] and result_board[2][1] == result_board[2][2] and sub_board[2][0] != emptynum):
+        return result_board[2][0];
 
-	# horizontal
-	if (result_board[0][0] == result_board[1][0] and result_board[1][0] == result_board[2][0] and sub_board[0][0] != emptynum):
-		return result_board[0][0];
-	if (result_board[0][1] == result_board[1][1] and result_board[1][1] == result_board[2][1] and sub_board[0][1] != emptynum):
-		return result_board[0][1];
-	if (result_board[0][2] == result_board[1][2] and result_board[1][2] == result_board[2][2] and sub_board[0][2] != emptynum):
-		return result_board[0][2];
+    # horizontal
+    if (result_board[0][0] == result_board[1][0] and result_board[1][0] == result_board[2][0] and sub_board[0][0] != emptynum):
+        return result_board[0][0];
+    if (result_board[0][1] == result_board[1][1] and result_board[1][1] == result_board[2][1] and sub_board[0][1] != emptynum):
+        return result_board[0][1];
+    if (result_board[0][2] == result_board[1][2] and result_board[1][2] == result_board[2][2] and sub_board[0][2] != emptynum):
+        return result_board[0][2];
 
-	# diagonals
-	if (result_board[0][0] == result_board[1][1] and result_board[1][1] == result_board[2][2] and sub_board[1][1] != emptynum):
-		return result_board[0][0];
-	if (result_board[2][0] == result_board[1][1] and result_board[1][1] == result_board[0][2] and sub_board[1][1] != emptynum):
-		return result_board[2][0];
+    # diagonals
+    if (result_board[0][0] == result_board[1][1] and result_board[1][1] == result_board[2][2] and sub_board[1][1] != emptynum):
+        return result_board[0][0];
+    if (result_board[2][0] == result_board[1][1] and result_board[1][1] == result_board[0][2] and sub_board[1][1] != emptynum):
+        return result_board[2][0];
 
-	for row in result_board:
-		for num in row:
-			if num == emptynum:
-				return num
+    for row in result_board:
+        for num in row:
+            if num == emptynum:
+                return num
 
-	# return 3 if full
-	return 3
+    # return 3 if full
+    return 3
 
 
 '''
@@ -194,17 +194,16 @@ printboard(test_board)
 
 is_free, valid_moves = get_valid_moves(test_board,9,'W')
 for i in range(3):
-	for j in range(3):
-		valid_moves[i][j] = zip(*valid_moves[i][j])
+    for j in range(3):
+        valid_moves[i][j] = zip(*valid_moves[i][j])
 valid_moves = zip(*valid_moves)
 printboard(valid_moves)
 
 is_free, valid_moves = get_valid_moves(test_board,2,'W')
 
 for i in range(3):
-	for j in range(3):
-		sys.stdout.write(str(valid_moves[i][j]) + ' ')
-	print
+    for j in range(3):
+        sys.stdout.write(str(valid_moves[i][j]) + ' ')
+    print
+<<<<<<< Updated upstream
 '''
-
-
--- python_files/program_7.py
+++ python_files/program_7.py
@@ -6,6 +6,7 @@ TOP_LEVEL = 7
 MIDDLE = 40
 
 emptychar = 'E'
+
 def algorithm(board, move, currColour, us):
     boardCheckResult = sub_board_is_full(board, move)
 
@@ -48,63 +49,69 @@ def getScore(board, checkRow, checkCol, currColour, us):
     return pointTotal;
 
 def empty(a):
-	if type(a) == list:
-		print a
-		for i in a:
-			if not empty(i):
-				return False
-	else:
-		print a
-		if a != 0:
-			return False
-	return True
+    if type(a) == list:
+        print a
+        for i in a:
+            if not empty(i):
+                return False
+    else:
+        print a
+        if a != 0:
+            return False
+    return True
+
 
 
 def get_move(time, string):
-	dt = datetime.now()
-	us = string[0]
-	where_move = string[1]
+    dt = datetime.now()
+    us = string[0]
+    where_move = string[1]
 
-	board = [[[[emptychar for x in range(3)] for y in range(3)] for z in range(3)] for k in range(3)]
+    board = [[[[emptychar for x in range(3)] for y in range(3)] for z in range(3)] for k in range(3)]
 
-	for i in range(3):
-		for j in range(3):
-			for k in range(3):
-				for l in range(3):
-					board[i][j][k][l] = string[27*i + 9*j + 3*k + l + 2]
-	board_utils.printboard(board)
-	if empty(board):
-		return MIDDLE
-	return "0" #bestmove
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    board[i][j][k][l] = string[27*i + 9*j + 3*k + l + 2]
 
-	#return x, y #false is small, true is big
+    board_utils.printboard(board)
+
+    if empty(board):
+        return MIDDLE
+    print "MOVE: " + where_move
+    best_move(us, us, int(where_move), board)
+    return 0
+
+    #return x, y #false is small, true is big
 
 
 def best_move(us, turn, move, board):
-	# is_free is when the move was a 9-type.
-	# returning True, and a 4D 3x3x3x3 array
-	# otherwise returns False, and returns 2D 3x3 array
-	# 2 is available,
-	# 1 is available but gives a free move,
-	# 0 is impossible
+    # is_free is when the move was a 9-type.
+    # returning True, and a 4D 3x3x3x3 array
+    # otherwise returns False, and returns 2D 3x3 array
+    # 2 is available,
+    # 1 is available but gives a free move,
+    # 0 is impossible
 
-	is_free, valid_moves = board_utils.get_valid_moves(board, move)
-	algorithm()
-	moves = board_utils.get_valid_moves(board, move)
-	#for move in moves:
+    is_free, valid_moves = board_utils.get_valid_moves(board, move)
+    #algorithm()
 
+    print valid_moves[0][0]
+    #algorithm()
+    print "Incomplete"
 
 '''
 def minimax(level, time_left, us, turn, move, board):
-	if level == 0:
-		return algorithm(move, turn, us, board)
-	elif terminal(board):
-		return tie_count_wins(us, board)
-	elif turn == us:
-		best_val = -1000000
-		moves = find_moves(turn, move, board)
-		if TOP_LEVEL  - 2 >=
-	else:
-		best_val = 1000000
-		moves = find_moves(turn, move, board)
+    if level == 0:
+        return algorithm(move, turn, us, board)
+    elif terminal(board):
+        return tie_count_wins(us, board)
+    elif turn == us:
+        best_val = -1000000
+        moves = find_moves(turn, move, board)
+        if TOP_LEVEL  - 2 >=
+    else:
+        best_val = 1000000
+        moves = find_moves(turn, move, board)
 '''
