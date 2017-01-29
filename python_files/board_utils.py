c625813bc697484dfcd515f1bfae611dddfac7f9
Kevin <kevinchencode@gmail.com>
2017-01-28 19:44:40 -0500

    put everything in python files

---
+++ python_files/board_utils.py
@@ -0,0 +1,134 @@
+import sys
+
+def opposite(currColour):
+    if currColour == '1':
+        return '2'
+    return '1'
+	
+# this will check if the entire 9x9 board is full.
+def board_is_full(board):
+	for sub_row in board:
+		for sub_board in sub_row:
+			for row in sub_board:
+				for char in row:
+					if char == '0':
+						return False
+
+	return True
+
+# this will check if a sub board is playable.
+# returns characters
+# will return the color that the board is owned by,
+# if tied in that board, it will return 'E'
+# if board is full it will return 'F'
+def sub_board_is_full(board, play_row, play_col):
+	sub_board = board[play_row][play_col]
+	# vertical
+	if (sub_board[0][0] == sub_board[0][1] and sub_board[0][1] == sub_board[0][2]):
+		return sub_board[0][0];
+	if (sub_board[1][0] == sub_board[1][1] and sub_board[1][1] == sub_board[1][2]):
+		return sub_board[1][0];
+	if (sub_board[2][0] == sub_board[2][1] and sub_board[2][1] == sub_board[2][2]):
+		return sub_board[2][0];
+
+	# horizontal
+	if (sub_board[0][0] == sub_board[1][0] and sub_board[1][0] == sub_board[2][0]):
+		return sub_board[0][0];
+	if (sub_board[0][1] == sub_board[1][1] and sub_board[1][1] == sub_board[2][1]):
+		return sub_board[0][1];
+	if (sub_board[0][2] == sub_board[1][2] and sub_board[1][2] == sub_board[2][2]):
+		return sub_board[0][2];
+
+	# diagonals
+	if (sub_board[0][0] == sub_board[1][1] and sub_board[1][1] == sub_board[2][2]):
+		return sub_board[0][0];
+	if (sub_board[2][0] == sub_board[1][1] and sub_board[1][1] == sub_board[0][2]):
+		return sub_board[2][0];
+
+	for row in sub_board:
+		for char in row:
+			if char == '0':
+				return char
+
+	return 'F'
+
+
+def printboard(board):
+	j = 0
+	for i in range(3):
+		for k in range(3):
+			for l1 in range(3):
+				sys.stdout.write(str(board[i][j][k][l1]) + ' ')
+			sys.stdout.write("  ")
+			for l2 in range(3):
+				sys.stdout.write(str(board[i][j+1][k][l2]) + ' ')
+			sys.stdout.write("  ")
+			for l3 in range(3):
+				sys.stdout.write(str(board[i][j+2][k][l3]) + ' ')
+
+			print ""
+
+		print ""
+
+# returns a 2-D array of valid moves
+# the array will be 3 by 3 size type int
+# play row and play col correspond to which sub_board
+# is to be played currently.
+
+
+# test board
+def get_valid_moves(board, play_row, play_col):
+
+	valid_moves = [[1 for x in range(3)] for y in range(3)]
+
+	sub_board = board[play_row][play_col]
+
+	for row_num in range(3):
+		for col_num in range(3):
+			# if empty
+			if sub_board[row_num][col_num] == '0':
+
+				if sub_board_is_full(board, row_num, col_num) == 'E':
+					valid_moves[row_num][col_num] = 2
+				else:
+					valid_moves[row_num][col_num] = 1
+			else:
+				valid_moves[row_num][col_num] = 0
+
+	return valid_moves
+
+
+# returns the character of who won the game
+# return the '0' character if no one has won yet
+# returns 'F' if board was full, and no one has won.
+def who_won(board):
+
+	result_board = [[sub_board_is_full(board,x,y) for x in range(3)] for y in range(3)]
+
+	if (result_board[0][0] == result_board[0][1] and result_board[0][1] == result_board[0][2]):
+		return result_board[0][0];
+	if (result_board[1][0] == result_board[1][1] and result_board[1][1] == result_board[1][2]):
+		return result_board[1][0];
+	if (result_board[2][0] == result_board[2][1] and result_board[2][1] == result_board[2][2]):
+		return result_board[2][0];
+
+	# horizontal
+	if (result_board[0][0] == result_board[1][0] and result_board[1][0] == result_board[2][0]):
+		return result_board[0][0];
+	if (result_board[0][1] == result_board[1][1] and result_board[1][1] == result_board[2][1]):
+		return result_board[0][1];
+	if (result_board[0][2] == result_board[1][2] and result_board[1][2] == result_board[2][2]):
+		return result_board[0][2];
+
+	# diagonals
+	if (result_board[0][0] == result_board[1][1] and result_board[1][1] == result_board[2][2]):
+		return result_board[0][0];
+	if (result_board[2][0] == result_board[1][1] and result_board[1][1] == result_board[0][2]):
+		return result_board[2][0];
+
+	for row in result_board:
+		for char in row:
+			if char == '0':
+				return char
+
+	return 'F'
---
+++ python_files/program_7.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+import board_utils
+
+TOP_LEVEL = 7
+MIDDLE = 40
+
+def algorithm(board, move, currColour, us):
+    boardCheckResult = sub_board_is_full(board, move)
+
+    if boardCheckResult == '0':
+        return getScore(board, move, currColour, us)
+
+    #We would have to make a bad move, by sending the opponent to a full or won section
+    if currColour == us:
+        return -1000000
+
+    #the opponent would make a bad move by sending us to a full or won section
+    else:
+        return 1000000
+
+def getScore(board, checkRow, checkCol, currColour, us):
+    pointTotal = 0;
+    for k2 in range(3):
+        for l2 in range(3):
+
+          curr = board[checkRow][checkCol][k2][l2]
+
+          if curr == us:
+            if k2 == 1 and l2 == 1:
+              pointTotal += 5
+            elif (k2+l2) % 2 == 0:
+              pointTotal += 3;
+            else:
+              pointTotal += 1;
+          elif curr == opposite(us):
+            if k2 == 1 and l2 == 1:
+              pointTotal -= 5;
+            elif (k2+l2) % 2 == 0:
+              pointTotal -= 3;
+            else:
+              pointTotal -= 1;
+
+
+          #printf("%c  %d\n", curr, pointTotal);
+
+    return pointTotal;
+
+def empty(a):
+	if type(a) == list:
+		for i in a:
+			empty(i)
+	else:
+		if a != 0:
+			return False
+	return True
+
+
+def get_move(time, string):
+	dt = datetime.now()
+	us = string[0]
+	where_move = string[1]
+	board = [[[[0]* 3]*3]*3]*3
+	for i in range(3):
+		for j in range(3):
+			for k in range(3):
+				for l in range(3):
+					print i, j, k , l, string[27*i + 9*j + 3*k + l + 2]
+					board[i][j][k][l] = string[27*i + 9*j + 3*k + l + 2]
+					print board[i][j][k][l]
+	board_utils.printboard(board)
+	if empty(board):
+		return MIDDLE
+	return "0" #bestmove
+
+
+def best_move(us, turn, move, board):
+	board_utils.get_valid_moves(board, )
+	algorithm()
+
+'''
+def minimax(level, time_left, us, turn, move, board):
+	if level == 0:
+		return algorithm(move, turn, us, board)
+	elif terminal(board):
+		return tie_count_wins(us, board)
+	elif turn == us:
+		best_val = -1000000
+		moves = find_moves(turn, move, board)
+		if TOP_LEVEL  - 2 >= 
+	else:
+		best_val = 1000000
+		moves = find_moves(turn, move, board)
+'''
