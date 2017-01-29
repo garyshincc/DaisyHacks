d3c596e11dd3303deccf8a6d4d45e874a7977e3e
Kevin <kevinchencode@gmail.com>
2017-01-29 06:10:31 -0500

    made some changes

--- python_files/program_7.py
+++ python_files/program_7.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 import board_utils
 import numpy
+import random
+from copy import copy, deepcopy
 
-TOP_LEVEL = 7
+TOP_LEVEL = 4
 MIDDLE = 40
 
-emptychar = 'E'
+
+
 def algorithm(board, move, currColour, us):
     boardCheckResult = sub_board_is_full(board, move)
 
@@ -47,53 +50,89 @@ def getScore(board, checkRow, checkCol, currColour, us):
 
     return pointTotal;
 
+
 def empty(a):
 	if type(a) == list:
 		for i in a:
 			if not empty(i):
 				return False
 	else:
-		if a != '0':
+		if a != 0:
 			return False
 	return True
 
-
 def get_move(time, string):
 	dt = datetime.now()
-	us = string[0]
-	where_move = string[1]
+	us = int(string[0])
+	where_move = int(string[1])
 
-	board = [[[[emptychar for x in range(3)] for y in range(3)] for z in range(3)] for k in range(3)]
+	board = [[[[0 for x in range(3)] for y in range(3)] for z in range(3)] for k in range(3)]
 
 	for i in range(3):
 		for j in range(3):
 			for k in range(3):
 				for l in range(3):
-					board[i][j][k][l] = string[27*i + 9*j + 3*k + l + 2]
+					board[i][j][k][l] = int(string[27*i + 9*j + 3*k + l + 2])
 	board_utils.printboard(board)
 	if empty(board):
 		print 
 		return MIDDLE
-	return "0" #bestmove
+	return  0 #bestmove(us, dt, move, board)
 
 	#return x, y #false is small, true is big
+'''def monte_carlo(is_free, valid_moves):
+	if is_free:
+		return valid_moves
+	else:
+		for i in range(3):
+			for j in range(3):
+				if valid_moves[i][j] == 2 and not random.getrandbits(1): # makes python get a random bool
+					valid_moves[i][j] = 3
+
+def copy_board(board):
+	new_board = deepcopy(board)
+	return new_board
 
 
-def best_move(us, turn, move, board):
+
+def best_move(us, time, move, board):
 	# is_free is when the move was a 9-type.
 	# returning True, and a 4D 3x3x3x3 array
 	# otherwise returns False, and returns 2D 3x3 array
+	# 3 is available but suppressed by monte carlo
 	# 2 is available,
 	# 1 is available but gives a free move,
-	# 0 is impossible
+	# 0 is someone is already there
 
 	is_free, valid_moves = board_utils.get_valid_moves(board, move)
-	algorithm()
-	moves = board_utils.get_valid_moves(board, move)
-	#for move in moves:
+	maximum = -1000000
+	bestmove = -1
+	move_counter = 0
+	if(is_free):
+		for i in range(3):
+			for j in range(3):
+				for k in range(3):
+					for l in range(3):
+						if(valid_moves[i][j][k][l] == 2):
+							move_counter+=1
+							new_board = copy_board(board)
+							new_board[i][j][k][l] = us  #made the move
+							current_score = minimax(TOP_LEVEL, time, us, us, move, new_board)
+							if current_score > maximum:
+								bestmove = 27*i + 9*j + 3*k + l
+								maximum = current_score
+
+	else:
+		i = move / 3
+		j = move % 3
+		move_counter = 0
+		for k in range(3):
+			for l in range(3):
+				if valid_moves[k][l] == 2:
 
 
-'''
+
+
 def minimax(level, time_left, us, turn, move, board):
 	if level == 0:
 		return algorithm(move, turn, us, board)
@@ -101,8 +140,14 @@ def minimax(level, time_left, us, turn, move, board):
 		return tie_count_wins(us, board)
 	elif turn == us:
 		best_val = -1000000
-		moves = find_moves(turn, move, board)
-		if TOP_LEVEL  - 2 >=
+		is_free, valid_moves = board_utils.get_valid_moves(board, move)
+		monte_carlo(is_free, valid_moves)
+		if is_free: 
+			for i in range(3):
+				for j in range(3):
+					for k in range(3):
+						for l in range(3):
+
 	else:
 		best_val = 1000000
 		moves = find_moves(turn, move, board)
